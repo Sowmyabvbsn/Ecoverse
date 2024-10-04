0dc8a3216eba1ff5f2592ad8b2f050f8b0929443
Tariq-act <khantarikact@gmail.com>
2024-10-05 00:41:06 +0530

    Product table UI

--- app/(root)/admin/dashboard/page.tsx
+++ app/(root)/admin/dashboard/page.tsx
@@ -1,30 +1,11 @@
+"use client";
+
+import ProductManagement from "@/components/Admin/ProductManagement";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { BarChart, List, Package, ShoppingBag, Users } from "lucide-react";
+import { useState } from "react";
+
 // Mock data
-const products = [
-  {
-    id: 1,
-    name: "Eco-Friendly Water Bottle",
-    price: 25,
-    category: "Home",
-    status: "Active",
-    badges: ["Eco-Friendly"],
-  },
-  {
-    id: 2,
-    name: "Bamboo Toothbrush Set",
-    price: 15,
-    category: "Personal Care",
-    status: "Active",
-    badges: ["Biodegradable"],
-  },
-  {
-    id: 3,
-    name: "Solar Power Bank",
-    price: 50,
-    category: "Tech",
-    status: "Pending",
-    badges: ["Energy Efficient"],
-  },
-];
 
 const orders = [
   {
@@ -49,8 +30,6 @@ const orders = [
     date: "2023-05-30",
   },
 ];
-
-const categories = ["Home", "Personal Care", "Tech", "Fashion", "Food"];
 
 const users = [
   {
@@ -77,11 +56,41 @@ const users = [
 ];
 
 export default function AdminDashboard() {
+  const [activeTab, setActiveTab] = useState("products");
+  const [isAddProductDialogOpen, setIsAddProductDialogOpen] = useState(false);
   return (
     <div>
       <div className='min-h-screen bg-green-50 py-8'>
         <div className='container px-4'>
-          <h1 className='page-heading'></h1>
+          <h1 className='page-heading'>Admin Dashboard</h1>
+
+          <Tabs value={activeTab} onValueChange={setActiveTab}>
+            <TabsList className='w-full grid grid-cols-5 mb-8 '>
+              <TabsTrigger className='flex items-center' value='products'>
+                <Package className='w-4 h-4 mr-2' /> Products
+              </TabsTrigger>
+              <TabsTrigger className='flex items-center' value='orders'>
+                <ShoppingBag className='w-4 h-4 mr-2' /> Orders
+              </TabsTrigger>
+              <TabsTrigger className='flex items-center' value='categories'>
+                <List className='w-4 h-4 mr-2' /> Categories
+              </TabsTrigger>
+              <TabsTrigger className='flex items-center' value='users'>
+                <Users className='w-4 h-4 mr-2' /> Users
+              </TabsTrigger>
+              <TabsTrigger className='flex items-center' value='reports'>
+                <BarChart className='w-4 h-4 mr-2' /> Reports
+              </TabsTrigger>
+            </TabsList>
+
+            <TabsContent value='products'>
+              <ProductManagement />
+            </TabsContent>
+            <TabsContent value='order'></TabsContent>
+            <TabsContent value='users'></TabsContent>
+            <TabsContent value='categories'></TabsContent>
+            <TabsContent value='reports'></TabsContent>
+          </Tabs>
         </div>
       </div>
     </div>
---
+++ components/Admin/ProductManagement.tsx
@@ -0,0 +1,114 @@
+"use client";
+import React, { useState } from "react";
+import { Button } from "../ui/button";
+import { Edit, Plus, Search, Trash2 } from "lucide-react";
+import { Input } from "../ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+
+const categories = ["Home", "Personal Care", "Tech", "Fashion", "Food"];
+const products = [
+  {
+    id: 1,
+    name: "Eco-Friendly Water Bottle",
+    price: 25,
+    category: "Home",
+    status: "Active",
+    badges: ["Eco-Friendly"],
+  },
+  {
+    id: 2,
+    name: "Bamboo Toothbrush Set",
+    price: 15,
+    category: "Personal Care",
+    status: "Active",
+    badges: ["Biodegradable"],
+  },
+  {
+    id: 3,
+    name: "Solar Power Bank",
+    price: 50,
+    category: "Tech",
+    status: "Pending",
+    badges: ["Energy Efficient"],
+  },
+];
+
+const ProductManagement = () => {
+  const [isAddProductDialogOpen, setIsAddProductDialogOpen] = useState(false);
+  return (
+    <div>
+      <div className='flex justify-between items-center mb-4'>
+        <h2 className='text-2xl font-bold'>Product Management</h2>
+        <Button onClick={() => setIsAddProductDialogOpen(true)}>
+          <Plus /> Add Product
+        </Button>
+      </div>
+
+      <div className='flex items-center space-x-2 mb-4'>
+        <Input placeholder='Search products...' className='bg-white' />
+        <Button variant={"outline"}>
+          <Search className='mr-2 h-4 w-4 ' /> Search
+        </Button>
+        <Select>
+          <SelectTrigger className='w-[180px] bg-white'>
+            <SelectValue placeholder='Filter by Category' />
+          </SelectTrigger>
+          <SelectContent>
+            {categories.map((category) => (
+              <SelectItem key={category} value={category.toLowerCase()}>
+                {category}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
+
+      <Table>
+        <TableHeader>
+          <TableRow>
+            {["Name", "Price", "Category", "Status", "Badge", "Actions"].map(
+              (head) => (
+                <TableHead key={head}>{head}</TableHead>
+              )
+            )}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {products.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>{product.price}</TableCell>
+              <TableCell>{product.category}</TableCell>
+              <TableCell>{product.status}</TableCell>
+              <TableCell>{product.badges}</TableCell>
+              <TableCell>
+                <Button variant={"ghost"} size={"sm"}>
+                  <Edit className='w-4 h-4' />
+                </Button>
+                <Button variant={"ghost"} size={"sm"}>
+                  <Trash2 className='w-4 h-4' />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default ProductManagement;
