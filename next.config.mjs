518aa799243b34a3ab6a8b16842f928901f768fb
Tariq-act <khantarikact@gmail.com>
2024-11-16 20:27:12 +0530

    prisma 5

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,16 @@
 const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com','res.cloudinary.com']
-    }
+    },
+    output:'standalone',
+    redirects: async () => [
+        {
+          source: '/api/auth',
+          destination: '/api/auth/[...nextauth]',
+          permanent: false, // For dynamic routes, provide fallback support
+        },
+      ],
+      reactStrictMode: true,
     
 };
 
